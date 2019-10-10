35762e27f7c07feb275eb22f65431ae9e6bbd9d6
Chelsea Smith <cms11231@gmail.com>
2019-10-10 10:23:23 -0600

    Correct list_quiz

--- list_quiz.rb
+++ list_quiz.rb
@@ -1,17 +1,16 @@
-# # Three even?
-# def three_even?(list)
-#     even_numbers = []
-#     list.size.times do |i|
-#         even_numbers.push(i % 2)
-#         string = even_numbers[i..i+3]
-#         if string == [0, 0, 0]
-#             return true
-#         else
-#             return false
-#         end
-#     end
-
-# end
+# Three even?
+def three_even?(list)
+    list.size.times do |i|
+        if list[i] % 2 == 0 && list[i + 1] % 2 == 0 && list[i + 2] % 2 == 0 # I used this code to determine if the number the loop is focusing on is an even number as long with the numbers that come after it, it will return a true.
+            return true
+        else # If the above does not work then it will return false becuase of an odd number.
+            return false
+        end
+    end 
+    if list.size == 0 # I added this if loop to make sure that if the list is empty it returns a false
+        return false
+    end
+end
 
 # puts three_even?([2, 1, 3, 5]) #false
 # puts three_even?([2, 4, 12, 5]) #true
@@ -30,15 +29,17 @@ def bigger_two(list1, list2)
     sum = 0
     sum2 = 0
     list1.each do |i|
-        sum = i + sum
+        sum += i # I just made the addition a little easier to read
     end
-    list2.each do |i|
-        sum2 = i + sum
+    list2.each do |n|
+        sum2 += n # I just made the addition a little easier to read
     end
     if sum > sum2
         print list1
-    else
+    elsif sum < sum2
         print list2
+    elsif sum == sum2 # I forgot that if they were equal it had to return the first one
+        print list1
     end
 end
 
@@ -59,7 +60,7 @@ def series_up(num)
     print string
 end
 
-puts series_up(1) #[1]
-puts series_up(2) #[1, 1, 2]
-puts series_up(3) #[1, 1, 2, 1, 2, 3]
-puts series_up(4) #[1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
+# puts series_up(1) #[1]
+# puts series_up(2) #[1, 1, 2]
+# puts series_up(3) #[1, 1, 2, 1, 2, 3]
+# puts series_up(4) #[1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
